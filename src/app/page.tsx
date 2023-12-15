ed00a40798ceeb7bdce568987ce9075c3f5ecd6c
BonPato98 <bonfiglipatricio12@hotmail.com>
2023-12-15 18:27:08 -0300

    feat(home) added text and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-base lg:flex">
-        <p>Portfolio day 1</p>
+    <main className="flex min-h-screen flex-col items-start justify-between p-24">
+      <div className="flex z-10 max-w-5xl w-auto items-start justify-around gap-5 font-roboto text-base flex-col">
+        <h1 className="text-6xl font-bold leading-snug">Hola, me llamo <br /> <p className="text-purple-500">PATRICIO BONFIGLI</p></h1>
+        <h2 className="text-4xl">Soy un Desarrollador Front End</h2>
       </div>
     </main>
   )
