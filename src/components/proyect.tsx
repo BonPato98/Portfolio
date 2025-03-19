9b485d8b952fec46ce18d5671334d58b86118b85
BonPato98 <bonfiglipatricio12@hotmail.com>
2025-03-19 16:42:30 -0300

    update(changed style of cards)

--- src/components/proyect.tsx
+++ src/components/proyect.tsx
@@ -10,13 +10,14 @@ interface ProyectProps {
 export default function Proyect ({title, preview} : ProyectProps) : JSX.Element {
 
     return (
-        <article className="p-4 bg-purple-800 rounded">
-            <header className="text-xl font-bold">
-                {title}
-            </header>
-            <Image src={preview || DefaultPreview} height={264} alt="preview of the proyect"/>
-            <main className="">
-                Soy una card para el proyecto {title}
+        <article className="bg-gray-800 rounded grid grid-cols-2 justify-items-center max-h-lg max-w-lg">
+            <Image className="aspect-16/9 object-cover" src={preview || DefaultPreview} alt="preview of the proyect"/>
+            <main className="p-6 overflow-hidden">
+                <header className="font-bold text-purple-400 pb-6">
+                    {title}
+                </header>
+                    Soy una card para el proyecto {title}
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laborum beatae provident sunt velit voluptatem ratione, esse nisi suscipit voluptate ad reiciendis rerum consequatur rem facilis repellendus eveniet, doloremque aperiam.
             </main>
         </article>
     )
